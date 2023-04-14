0750e7c608636241e721de3bb44a1454c5306e31
discphy <dinophy@nate.com>
2023-04-14 18:17:36 +0900

    feat(init): add
    Project init

---
+++ src/main/java/com/fcprovin/batch/ChukCheckBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fcprovin.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukCheckBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChukCheckBatchApplication.class, args);
+	}
+
+}
